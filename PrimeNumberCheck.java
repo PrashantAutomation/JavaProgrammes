35b41ea7f093b7908d1de80c6908092be21cc23d
prashant <prashant1.dubey@paytm.com>
2020-05-02 23:47:51 +0530

    To check a number is prime or not

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,23 @@
+
+public class PrimeNumberCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 179;
+		boolean isPrime = true;
+		for(int i=2; i<num; i++)
+		{
+			if(num%i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		if(isPrime)
+		{
+		System.out.println(num + " Is prime");
+		}
+		else
+			System.out.println(num + " is not a prime number");
+		}
+}
